c490c86fce2cd372955087024a0d2f131e2c5585
unknown <darknemiroff@gmail.com>
2023-11-06 14:46:44 +0400

    buttons loop

--- app.js
+++ app.js
@@ -3,3 +3,55 @@ document.getElementById('pixi-container').appendChild(app.view);
 window.addEventListener('resize', () => {
     app.renderer.resize(window.innerWidth, window.innerHeight);
 });
+
+let elementsAndColors = [
+    { number: 1, color: 0xFF0000 }, 
+    { number: 2, color: 0x00FF00 }, 
+    { number: 3, color: 0x0000FF }, 
+    { number: 4, color: 0xFFFF00 }, 
+    { img: 'img/icon1.png', color: 0000000 }, 
+    { number: 5, color: 0xFF00FF }, 
+    { number: 6, color: 0x00FFFF }, 
+    { number: 7, color: 0xFFA500 }, 
+    { number: 8, color: 0x800080 },  
+    { number: 9, color: 0x00FFFF }, 
+    { number: 10, color: 0xFFA500 }, 
+    { img: 'img/icon2.png', color: 0000000 }, 
+    { number: 11, color: 0xFFA500 },
+    
+];
+
+
+let buttonDiv = document.getElementById('btn-cont');
+let chooseNumber = null;
+
+elementsAndColors.forEach((item, index) => {
+    if (item.img) {
+        const button = document.createElement('button');
+        const img = document.createElement('img');
+        img.src = item.img;
+        img.width = 20;
+        img.height = 20;
+        button.appendChild(img.cloneNode(true)); 
+        button.addEventListener('click', () => {
+            chooseNumber = index;
+            let choosePrize = document.getElementById('choose_prize');
+            const textElement = document.createElement('span');
+            textElement.textContent = 'Pressed Numbers: ';
+            choosePrize.innerHTML = ''; 
+            choosePrize.appendChild(textElement);
+            choosePrize.appendChild(img.cloneNode(true));
+        });
+        buttonDiv.appendChild(button);
+    } else {
+        const button = document.createElement('button');
+        button.textContent = `${item.number}`;
+        button.addEventListener('click', () => {
+            chooseNumber = index;
+            const contentDiv = document.getElementById('choose_prize');
+            contentDiv.innerHTML = 'Pressed Numbers: ' + item.number;
+        });
+        buttonDiv.appendChild(button);
+    }
+});
+
